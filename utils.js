5f05b7d8a21424711ec10c350cb6d53796678055
Bruno Alves de Souza <b.alves@kroton.com.br>
2024-09-26 13:57:51 -0300

    teste

--- utils.js
+++ utils.js
@@ -1,4 +1,7 @@
-function sumThoseNumbers(num1, num2) {
+function sum(num1, num2) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
     return num1 + num2;
 }
-// função para somar 2 números
+// Function to sum two numbers
